8084df8585e6dd69e105e7d2f4083cd39ae14476
Himanshu dua <hdua@q3tech.com>
2017-08-22 12:25:15 +0530

    Private url change

--- credit_computing_machine/app/views.py
+++ credit_computing_machine/app/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.dispatch import receiver
-from privateurl.signals import privateurl_ok, privateurl_fail
 
-@receiver(privateurl_ok)
-def conf(request, obj, action, **kwargs):
-    import pdb
-    pdb.set_trace()
-    if action != 'conf':
-        return
+from django.http import HttpResponse
+import datetime
 
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
--- credit_computing_machine/privateurl/views.py
+++ credit_computing_machine/privateurl/views.py
@@ -1,22 +1,22 @@
 from django.http.response import Http404, HttpResponseRedirect
 from .models import PrivateUrl
 from .signals import privateurl_ok, privateurl_fail
+from app.views import  current_datetime
 
+dict_private_urls_view = {
+    'manage_credit_score':current_datetime}
 
 def privateurl_view(request, action, token):
     obj = PrivateUrl.objects.get_or_none(action, token)
     ok = False
     if not obj or not obj.is_available():
         # fail or expired
-        pass
-    else:
-        pass
+        raise Http404
+    elif dict_private_urls_view(action):
+        return current_datetime(request,token)
         # sucess case
-    for receiver, result in results:
-        if isinstance(result, dict):
-            if 'response' in result:
-                return result['response']
     if not ok:
         raise Http404
     return HttpResponseRedirect('/')
+
 
