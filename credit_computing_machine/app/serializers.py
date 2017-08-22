2508272d0312ce779c9ac7a320f6136494e09b25
Himanshu dua <hdua@q3tech.com>
2017-08-22 18:22:49 +0530

    Nested serializer for group

--- credit_computing_machine/app/serializers.py
+++ credit_computing_machine/app/serializers.py
@@ -5,10 +5,10 @@ from .models import CreditScore
 
 
 class CreditGroupCreateSerializer(serializers.ModelSerializer):
-
     '''
     Serializer for CreditGroup Create.
     '''
+
     class Meta:
         '''
         Serializer customization
@@ -19,13 +19,40 @@ class CreditGroupCreateSerializer(serializers.ModelSerializer):
 
 
 class CreditUserCreateSerializer(serializers.ModelSerializer):
-
     '''
     Serializer for CreditUser Create.
     '''
+
     class Meta:
         '''
         Serializer customization
         '''
         model = CreditUser
         fields = '__all__'
+class CreditUserSerializer(serializers.ModelSerializer):
+    '''
+    Serializer for CreditUser Create.
+    '''
+
+    class Meta:
+        '''
+        Serializer customization
+        '''
+        model = CreditUser
+        fields = '__all__'
+
+class CreditGroupSerializer(serializers.ModelSerializer):
+    '''
+    Serializer for CreditGroup Create.
+    '''
+    credit_users = CreditUserSerializer(many=True)
+
+    class Meta:
+        '''
+        Serializer customization
+        '''
+        model = CreditGroup
+        fields = ('id', 'name', 'credit_users')
+
+
+
--- credit_computing_machine/app/views.py
+++ credit_computing_machine/app/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-import datetime
 from .models import CreditGroup
+from .serializers import CreditGroupSerializer
 def manage_group(request,purl_id):
     credit_group = CreditGroup.objects.filter(privateurl__id=purl_id).first()
-    print(CreditGroup.objects.get_credit_all_user(credit_group.id))
-    html = "<html><body>It is now </body></html>"
-    return HttpResponse(html)
+    credit_group = CreditGroup.objects.filter(privateurl__id=purl_id).first()
+    response = CreditGroupSerializer(credit_group).data
+    return HttpResponse(str(response))
