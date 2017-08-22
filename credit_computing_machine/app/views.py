42cf9ff85aa91d1b1c0d88d188b0f96d9b1d58c0
Himanshu dua <hdua@q3tech.com>
2017-08-22 17:35:22 +0530

    Initialize manage group api

--- credit_computing_machine/app/views.py
+++ credit_computing_machine/app/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 import datetime
-
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+from .models import CreditGroup
+def manage_group(request,purl_id):
+    credit_group = CreditGroup.objects.filter(privateurl__id=purl_id).first()
+    print(CreditGroup.objects.get_credit_all_user(credit_group.id))
+    html = "<html><body>It is now </body></html>"
     return HttpResponse(html)
--- credit_computing_machine/privateurl/views.py
+++ credit_computing_machine/privateurl/views.py
@@ -1,10 +1,12 @@
 from django.http.response import Http404, HttpResponseRedirect
 from .models import PrivateUrl
-from .signals import privateurl_ok, privateurl_fail
-from app.views import  current_datetime
+from app.views import manage_group
 
 dict_private_urls_view = {
-    'manage_credit_score':current_datetime}
+    'manage_credit_score':manage_group,
+    'user_credit':manage_group,
+
+}
 
 def privateurl_view(request, action, token):
     obj = PrivateUrl.objects.get_or_none(action, token)
@@ -12,9 +14,9 @@ def privateurl_view(request, action, token):
     if not obj or not obj.is_available():
         # fail or expired
         raise Http404
-    elif dict_private_urls_view(action):
-        view = dict_private_urls_view(action)
-        return view(request,token)
+    elif dict_private_urls_view.get(action):
+        view = dict_private_urls_view.get(action)
+        return view(request,obj.id)
         # sucess case
     if not ok:
         raise Http404
