0943b5635f8d51fe1c336b14ad2da9cef076482d
Himanshu <duahimanshu100@gmail.com>
2017-08-30 23:37:10 +0530

    Email settngs

--- credit_computing_machine/credit_computing_machine/constants.py
+++ credit_computing_machine/credit_computing_machine/constants.py
@@ -1,8 +1,9 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'me@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'duahimanshu101@gmail.com'
+EMAIL_HOST_PASSWORD = 'clasmate'
 
 
 FRONT_END_ROOT_URL = 'http://localhost:8080'
+EMAIL_BACKEND = 'post_office.EmailBackend'
