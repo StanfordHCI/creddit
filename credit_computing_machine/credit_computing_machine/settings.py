64d0d6e5905ae39de518c2307ca89fefa70c2e18
Himanshu <duahimanshu100@gmail.com>
2017-08-31 22:26:37 +0530

    change prod db

--- credit_computing_machine/credit_computing_machine/settings.py
+++ credit_computing_machine/credit_computing_machine/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'credit_computing_machine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': 'hello',
+        'USER': 'hello',
+        'PASSWORD': 'changeme',
     }
 }
 
