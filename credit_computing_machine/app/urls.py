48104986ce1136b472e0f8583be82310d59c6ef3
himanshu <duahimanshu100@gmail.com>
2017-09-12 16:17:39 +0530

    Add new group count api

--- credit_computing_machine/app/urls.py
+++ credit_computing_machine/app/urls.py
@@ -3,6 +3,7 @@ from . import api
 
 
 urlpatterns = [
+    url(r'^(?i)api/groups/count/', api.CreditGroupCount.as_view()),
     url(r'^(?i)api/groups/CreditGroupCreateApi/', api.CreditGroupCreateApi.as_view()),
     url(r'^(?i)api/groups/CreditGroupRetrieveUpdateAPI/(?P<token>[\-a-zA-Z0-9]{1,64})$', api.CreditGroupRetrieveUpdateAPI.as_view()),
     url(r'^(?i)api/users/CreditUserScoresRetrieveUpdateAPI/(?P<token>[\-a-zA-Z0-9]{1,64})$', api.CreditUserScoresRetrieveUpdateAPI.as_view()),
