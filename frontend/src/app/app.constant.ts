4853fa370f43c0c387d2ac12461282aa88449392
agam <agam@girnarsoft.com>
2017-11-24 18:05:57 +0530

    Changes for SSL & New URL

--- frontend/src/app/app.constant.ts
+++ frontend/src/app/app.constant.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-   public static API_ENDPOINT = 'https://creddit.io/api';
+   public static API_ENDPOINT = 'https://creddit.stanford.edu/api';
    public static TOTAL_POINTS = 100;
 }
